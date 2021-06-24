22154cf4942874b0d08caa7dee8916333afffd8f
Marcos <marcosfnlr@gmail.com>
2021-06-24 00:57:17 -0300

    Add camera and player movement and aiming control

--- tiro/Base.gd
+++ tiro/Base.gd
@@ -1,11 +1,6 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,5 +12,5 @@ func _physics_process(delta):
 func process_input(delta):
 	
 	if Input.is_action_just_pressed("fire"):
-			$Pistol_Point.fire_weapon()
+			$Player/Rotate/Pistol_Point.fire_weapon()
 	# ----------------------------------
--- tiro/Weapon_Pistol.gd
+++ tiro/Weapon_Pistol.gd
@@ -22,24 +22,3 @@ func fire_weapon():
 	clone.global_transform = self.global_transform
 	clone.scale = Vector3(4, 4, 4)
 	clone.BULLET_DAMAGE = DAMAGE
-
-#func equip_weapon():
-#    if player_node.animation_manager.current_state == IDLE_ANIM_NAME:
-#        is_weapon_enabled = true
-#        return true
-#
-#    if player_node.animation_manager.current_state == "Idle_unarmed":
-#        player_node.animation_manager.set_animation("Pistol_equip")
-#
-#    return false
-#
-#func unequip_weapon():
-#    if player_node.animation_manager.current_state == IDLE_ANIM_NAME:
-#        if player_node.animation_manager.current_state != "Pistol_unequip":
-#            player_node.animation_manager.set_animation("Pistol_unequip")
-#
-#    if player_node.animation_manager.current_state == "Idle_unarmed":
-#        is_weapon_enabled = false
-#        return true
-#    else:
-#        return false
